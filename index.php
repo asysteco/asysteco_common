854fb530f75ca7a58b4003b9c2c77f787cb74343
asysteco <admin@asysteco.com>
2020-08-28 20:59:27 +0200

    edit profesor (Tutor)

--- index.php
+++ index.php
@@ -881,6 +881,27 @@ if(isset($_GET['ACTION']))
           {
             include_once($dirs['inc'] . 'valida_edit_profesor.php');
             $extras = '<link rel="stylesheet" href="css/login-style.css">';
+            $style "
+            #grupo-tutor-select {
+              background-color: #f6f6f6;
+              border: none;
+              color: #0d0d0d;
+              padding: 15px 32px;
+              text-align: center;
+              text-decoration: none;
+              display: inline-block;
+              font-size: 16px;
+              margin: 5px;
+              width: 85%;
+              border: 2px solid #f6f6f6;
+              -webkit-transition: all 0.5s ease-in-out;
+              -moz-transition: all 0.5s ease-in-out;
+              -ms-transition: all 0.5s ease-in-out;
+              -o-transition: all 0.5s ease-in-out;
+              transition: all 0.5s ease-in-out;
+              -webkit-border-radius: 5px 5px 5px 5px;
+              border-radius: 5px 5px 5px 5px;
+            }";
             include_once($dirs['inc'] . 'top-nav.php');
             include_once($dirs['inc'] . 'editar_profesor.php');
             include_once($dirs['inc'] . 'errors.php');
