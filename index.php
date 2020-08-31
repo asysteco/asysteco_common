55e30bc16db6f574ac3721bc6eebfc3f8217bed1
asysteco <admin@asysteco.com>
2020-08-31 20:10:36 +0200

    index edificios

--- index.php
+++ index.php
@@ -54,17 +54,40 @@ if(isset($_GET['ACTION']))
           if($class->compruebaCambioPass())
           {
             $act_home = 'active';
+            $scripts = '<link rel="stylesheet" href="css/qr-reader.css">';
+            $scripts .= '
+            <script type="text/javascript" src="js/jsqrcode/grid.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/version.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/detector.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/formatinf.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/errorlevel.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/bitmat.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/datablock.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/bmparser.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/datamask.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/rsdecoder.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/gf256poly.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/gf256.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/decoder.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/qrcode.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/findpat.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/alignpat.js"></script>
+            <script type="text/javascript" src="js/jsqrcode/databr.js"></script>
+            ';
+            include($dirs['inc'] . 'top-nav.php');
             switch ($_GET['OPT'])
             {
-              case 'value':
-                # code...
-                break;
+              case 'Edificio':
+                include($dirs['inc'] . 'home.php');
+              break;
               
               default:
-                # code...
-                break;
+                include($dirs['inc'] . 'home.php');
+              break;
             }
-            include($dirs['inc'] . 'home.php');
+
+            include($dirs['inc'] . 'errors.php');
+            include($dirs['inc'] . 'footer.php');
           }
           else
           {
@@ -618,99 +641,6 @@ if(isset($_GET['ACTION']))
           $MSG = "Debes iniciar sesión para realizar esta acción.";
           header("Refresh:2; url=index.php");
           include_once($dirs['inc'] . 'msg_modal.php');
-        }
-      break;
-
-      case 'Edificio':
-        if(isset($_GET['Numero']))
-        {
-          if($class->isLogged())
-          {
-            if($class->compruebaCambioPass())
-            {
-              $act_home = 'active';
-              $scripts = '
-              <script type="text/javascript" src="js/jsqrcode/grid.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/version.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/detector.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/formatinf.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/errorlevel.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/bitmat.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/datablock.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/bmparser.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/datamask.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/rsdecoder.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/gf256poly.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/gf256.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/decoder.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/qrcode.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/findpat.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/alignpat.js"></script>
-              <script type="text/javascript" src="js/jsqrcode/databr.js"></script>
-              ';
-              if($_SESSION['Perfil'] === 'Admin')
-              {
-                $style = '
-                    canvas {box-shadow: 4px 6px 15px grey; padding: 2px; border-radius: 10px;}
-                    .respuesta span {display: block; box-shadow: 4px 6px 15px grey; padding: 50px; border-radius: 10px; margin-top: 30px;}
-                    .filtro_edificio {
-                        margin-top: 50px;
-                    }
-                    #qreader {
-                        text-align: center;
-                    }
-                ';
-                include($dirs['inc'] . 'top-nav.php');
-                echo '<div class="container-fluid" style="margin-top:50px">';
-                  echo "<div class='row'>";
-                      echo "<div id='qreader' class='col-xs-12 col-md-4' >";
-                          echo "<h3>Fichaje</h3>";
-                          include($dirs['inc'] . 'qr-reader.php');
-                      echo "</div>";
-                      echo "<div class='col-xs-12 col-md-8' style='text-align: center;'>";
-                          include($dirs['inc'] . 'filtro-edif-guardias.php');
-                          include($dirs['inc'] . 'contenido-guardias.php');
-                      echo "</div>";
-                  echo "</div>";
-                echo "</div>";
-                include($dirs['inc'] . 'errors.php');
-                include($dirs['inc'] . 'footer.php');
-              }
-              else
-              {
-                $style = '
-                .filtro_edificio {
-                    margin-top: 50px;
-                }
-                ';
-                include($dirs['inc'] . 'top-nav.php');
-                echo '<div class="container-fluid" style="margin-top:50px">';
-                  echo "<div class='row'>";
-                    echo "<div class='col-xs-12' style='text-align: center;'>";
-                        include($dirs['inc'] . 'filtro-edif-guardias.php');
-                        include($dirs['inc'] . 'contenido-guardias.php');
-                    echo "</div>";
-                  echo "</div>";
-                echo "</div>";
-                include($dirs['inc'] . 'errors.php');
-                include($dirs['inc'] . 'footer.php');
-              }
-            }
-            else
-            {
-              header('Location: index.php?ACTION=primer_cambio');
-            }
-          }
-          else
-          {
-            $MSG = "Debes iniciar sesión para realizar esta acción.";
-            header("Refresh:2; url=index.php");
-            include_once($dirs['inc'] . 'msg_modal.php');
-          }
-        }
-        else
-        {
-          header("Location: url=index.php");
         }
       break;
 
