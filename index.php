1784e1e53ea480d5f68e11fe3ec48314db4f7e59
asysteco <admin@asysteco.com>
2020-08-31 20:13:21 +0200

    redirects

--- index.php
+++ index.php
@@ -78,7 +78,14 @@ if(isset($_GET['ACTION']))
             switch ($_GET['OPT'])
             {
               case 'Edificio':
+                if(isset($_GET['Numero']))
+                {
                 include($dirs['inc'] . 'home.php');
+                }
+                else
+                {
+                    header('Location: index.php');
+                }
               break;
               
               default:
