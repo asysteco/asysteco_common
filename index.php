16efcffcc02e8e1dbbc9b4ce852f87368e09a971
asysteco <admin@asysteco.com>
2020-08-31 14:29:52 +0200

    update

--- index.php
+++ index.php
@@ -318,26 +318,6 @@ if(isset($_GET['ACTION']))
         }
       break;
     
-      case 'update-horario':
-        if($class->isLogged())
-        {
-          if($class->compruebaCambioPass())
-          {
-            include_once($dirs['inc'] . 'actualiza.php');
-          }
-          else
-          {
-            header('Location: index.php?ACTION=primer_cambio');
-          }
-        }
-        else
-        {
-          $MSG = "Debes iniciar sesión para realizar esta acción.";
-          header("Refresh:2; url=index.php");
-          include_once($dirs['inc'] . 'msg_modal.php');
-        }
-      break;
-    
       case 'asistencias':
         if($class->isLogged())
         {
