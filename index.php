21d8a806aa7a5772041e92767a98f08db3b5987d
asysteco <admin@asysteco.com>
2020-09-03 15:13:57 +0200

    acepta guiones en nombre

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 
 $subrootsplit = preg_split('/\//', $_SERVER['REQUEST_URI']);
 $subroot = '/' . $subrootsplit[1];
-preg_match('/^\/[A-Z]+$/i', $subroot) ? $subroot = $subroot : $subroot = '' ;
+preg_match('/^\/[A-Z-]+$/i', $subroot) ? $subroot = $subroot : $subroot = '' ;
 
 $Titulo = preg_split('/\//', $subroot);
 $Titulo = $Titulo[1];
