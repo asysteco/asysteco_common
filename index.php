7522141005a114770f58e2827f8b1195311eac41
Jokerrdl <ruizdeluna15@gmail.com>
2020-10-05 19:06:57 +0200

    QR-reader implemented

--- index.php
+++ index.php
@@ -661,25 +661,29 @@ if(isset($_GET['ACTION']))
           {
             $act_guardias = 'active';
             $scripts = '<link rel="stylesheet" href="css/qr-reader.css">';
-            $scripts .= '
-            <script type="text/javascript" src="js/jsqrcode/grid.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/version.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/detector.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/formatinf.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/errorlevel.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/bitmat.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/datablock.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/bmparser.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/datamask.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/rsdecoder.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/gf256poly.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/gf256.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/decoder.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/qrcode.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/findpat.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/alignpat.js"></script>
-            <script type="text/javascript" src="js/jsqrcode/databr.js"></script>
-            ';
+
+            if (! $options['QR-reader']) {
+              $scripts .= '
+              <script type="text/javascript" src="js/jsqrcode/grid.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/version.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/detector.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/formatinf.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/errorlevel.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/bitmat.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/datablock.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/bmparser.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/datamask.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/rsdecoder.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/gf256poly.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/gf256.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/decoder.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/qrcode.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/findpat.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/alignpat.js"></script>
+              <script type="text/javascript" src="js/jsqrcode/databr.js"></script>
+              ';
+            }
+            
             include_once($dirs['inc'] . 'top-nav.php');
             include($dirs['inc'] . 'home.php');
             include($dirs['inc'] . 'errors.php');
@@ -720,7 +724,7 @@ if(isset($_GET['ACTION']))
           <script>
             var userAgent = navigator.userAgent.toLowerCase();
             var isSupportedBrowser = (/armv.* raspbian chromium/i).test(userAgent);
-            if(! isSupportedBrowser)
+            if(isSupportedBrowser)
             {
               location.href = "index.php";
             }
