59e6f85322a09aa0b37760e63efdc5254516a07d
varo45 <alvaromelkunas9@gmail.com>
2020-12-22 22:58:51 +0100

    Scroller con espaciado superior como variable

--- js/scroller-interaction.js
+++ js/scroller-interaction.js
@@ -2,12 +2,13 @@
 var scrollTime = 1000; // Tiempo de scroll
 var wait = 9000; // Tiempo que tarda entre cada scroll
 
+var whiteSpaceHeight = 21; // Altura del espaciado superior en px
 var top = 'fila_0'; // Fila Inicial
-var bottom = 'fila_15'; // Fila Final
+var bottom = 'fila_16'; // Fila Final
 
 function scrollDown() {
   $(".scroller").animate({
-    scrollTop: $("#"+bottom).offset().top
+    scrollTop: $("#"+bottom).offset().top-whiteSpaceHeight
   }, scrollTime)
 };
 
