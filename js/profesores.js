b76e5de475101befdc6f522968b92cba2bad7698
Jokerrdl <ruizdeluna15@gmail.com>
2021-01-21 18:23:42 +0100

    JS_Modal_Horario_Profesor

--- js/profesores.js
+++ js/profesores.js
@@ -1,8 +1,7 @@
-<script>
-    $('.row_show').on('click', function() {
-        id = $(this).parent().attr('id').split('_'),
-        id = id[1],
-        $("#horario").load('index.php?ACTION=horarios&OPT=profesor&profesor=' + id),
-        $("html, body").animate({ scrollTop: 0 })
-    });
-</script>
+$('.row-show').on('click', function() {
+    console.log('pulsado');
+    id = $(this).parent().attr('id').split('_'),
+    id = id[1],
+    $("#horario").load('index.php?ACTION=horarios&OPT=profesor&profesor=' + id),
+    $('#modal-horario').modal('show')
+});
