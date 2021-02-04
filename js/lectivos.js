35afaa2186f08b511c239eff5ad8a07df09ca879
Jokerrdl <ruizdeluna15@gmail.com>
2021-02-04 18:20:58 +0100

    lectivos.js

--- js/lectivos.js
+++ js/lectivos.js
@@ -1,6 +1,6 @@
 $('#datepicker_ini, #datepicker_fin, #datepicker_ini_fest, #datepicker_fin_fest').keypress(function(e) {
     e.preventDefault()
 });
-// $(document).on('click', '.lectivo', function (e) {
-//     $('#modal-calendario').modal('show');
-// });
+$(document).on('click', '.lectivo', function (e) {
+    $('#modal-calendario').modal('show');
+});
